70cbaa392c05c707393e52bb58badd9c60cecc73
kindolechris <51138121+kindolechris@users.noreply.github.com>
2020-01-22 22:31:45 -0800

    updates on login form

--- app/src/main/java/com/dsetanzania/dse/MainActivity.java
+++ app/src/main/java/com/dsetanzania/dse/MainActivity.java
@@ -1,13 +1,28 @@
 package com.dsetanzania.dse;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // force full screen mode
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
     }
+
 }
